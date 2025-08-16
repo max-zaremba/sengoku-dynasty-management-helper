08997d738acdeeeb5e554a248f080682c4cbfbd5
Max Zaremba <max.zaremba@gmail.com>
2025-08-16 17:56:16 -0400

    remove duplicated sort icon logic

--- components/item-table/item-table-header-row.tsx
+++ components/item-table/item-table-header-row.tsx
@@ -2,9 +2,9 @@
 
 import { Column, ColumnKey, tableColumns } from '@/lib/columns';
 import { Button } from '../ui/button';
-import { ArrowUpDown, ArrowUp, ArrowDown } from 'lucide-react';
 import React from 'react';
-import { SORT_DIRECTION, SortDirection } from '@/lib/types/sort-types';
+import { SortDirection } from '@/lib/types/sort-types';
+import { SortIcon } from './sort-icons';
 
 interface ItemTableHeaderRowProps {
 	sortField: ColumnKey | undefined;
@@ -12,20 +12,6 @@ interface ItemTableHeaderRowProps {
 	onSortClick: (field: ColumnKey) => void;
 	visibleHeaders?: string[];
 }
-
-const getSortIcon = (
-	columnKey: ColumnKey,
-	sortField: ColumnKey | undefined,
-	sortDirection: SortDirection,
-) => {
-	if (columnKey !== sortField || sortDirection == SORT_DIRECTION.NONE) {
-		return <ArrowUpDown className='w-4 h-4 inline ml-1' />;
-	} else if (sortDirection === SORT_DIRECTION.ASCENDING) {
-		return <ArrowUp className='w-4 h-4 inline ml-1' />;
-	} else {
-		return <ArrowDown className='w-4 h-4 inline ml-1' />;
-	}
-};
 
 const ItemTableColumn = (
 	props: ItemTableHeaderRowProps & {
@@ -45,11 +31,7 @@ const ItemTableColumn = (
 			className='font-semibold p-0'
 		>
 			{props.column.name}{' '}
-			{getSortIcon(
-				props.column.key,
-				props.sortField,
-				props.sortDirection,
-			)}
+			<SortIcon columnKey={props.column.key} sortField={props.sortField} sortDirection={props.sortDirection} />
 		</Button>
 	);
 };
@@ -58,7 +40,7 @@ export const ItemTableHeaderRow = (props: ItemTableHeaderRowProps) => (
 	<tr className='border-b bg-muted/50'>
 		{tableColumns.map((column) =>
 			!props.visibleHeaders ||
-			props.visibleHeaders.includes(column.key) ? (
+				props.visibleHeaders.includes(column.key) ? (
 				<th key={column.key} className='py-1 text-center'>
 					<ItemTableColumn {...props} column={column} />
 				</th>
--- components/item-table/sort-icons.tsx
+++ components/item-table/sort-icons.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 import { ColumnKey } from '@/lib/columns';
 import { SortDirection, SORT_DIRECTION } from '@/lib/types/sort-types';
 
-export const getSortIcon = (
-	columnKey: ColumnKey,
-	sortField: ColumnKey | undefined,
-	sortDirection: SortDirection,
-) => {
-	if (columnKey !== sortField || sortDirection == SORT_DIRECTION.NONE) {
-		return <ArrowUpDown className='w-4 h-4 inline ml-1' />;
-	} else if (sortDirection === SORT_DIRECTION.ASCENDING) {
-		return <ArrowUp className='w-4 h-4 inline ml-1' />;
-	} else {
-		return <ArrowDown className='w-4 h-4 inline ml-1' />;
-	}
+export const SortIcon = (props: {
+    columnKey: ColumnKey,
+    sortField: ColumnKey | undefined,
+    sortDirection: SortDirection,
+}) => {
+    if (props.columnKey !== props.sortField || props.sortDirection == SORT_DIRECTION.NONE) {
+        return <ArrowUpDown className='w-4 h-4 inline ml-1' />;
+    } else if (props.sortDirection === SORT_DIRECTION.ASCENDING) {
+        return <ArrowUp className='w-4 h-4 inline ml-1' />;
+    } else {
+        return <ArrowDown className='w-4 h-4 inline ml-1' />;
+    }
 };
